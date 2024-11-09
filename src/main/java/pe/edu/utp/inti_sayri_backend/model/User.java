f05c418dcf1c7aad32d112a472f76e9cb5684284
Emmanuel-Rivera-G <U22222056@utp.edu.pe>
2024-11-09 00:19:46 -0500

    Actualizacion el User.java.

--- src/main/java/pe/edu/utp/inti_sayri_backend/model/User.java
+++ src/main/java/pe/edu/utp/inti_sayri_backend/model/User.java
@@ -10,6 +10,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
     private String nombreCompleto;
     private String correoElectronico;
     private String contrasena;   
