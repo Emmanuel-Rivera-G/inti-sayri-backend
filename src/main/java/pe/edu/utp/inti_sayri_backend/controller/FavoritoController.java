2089164ff70e8b052f451eb63f48a91d1f3d1a97
Emmanuel-Rivera-G <U22222056@utp.edu.pe>
2024-11-10 00:45:45 -0500

    Actualizando Favorito Controller

--- src/main/java/pe/edu/utp/inti_sayri_backend/controller/FavoritoController.java
+++ src/main/java/pe/edu/utp/inti_sayri_backend/controller/FavoritoController.java
@@ -5,19 +5,29 @@ import org.springframework.web.bind.annotation.*;
 import pe.edu.utp.inti_sayri_backend.service.FavoritoService;
 
 @RestController
-@RequestMapping("/api/favoritos")
+@RequestMapping("/api/loction-favoritos")
 public class FavoritoController {
 
     @Autowired
     private FavoritoService favoritoService;
 
     @PostMapping("/add")
-    public String addFavorito(@RequestParam Long userId, @RequestParam String locationId) {
+    public String addFavorito(@RequestParam(value = "user-id") Long userId, @RequestParam(value = "location-id") String locationId) {
+        return favoritoService.addFavorito(userId, locationId);
+    }
+    
+    @PostMapping("/{userId}/add/{locationId}")
+    public String addUrlPathFavorito(@PathVariable("userId") Long userId, @PathVariable(value = "locationId") String locationId) {
         return favoritoService.addFavorito(userId, locationId);
     }
 
     @DeleteMapping("/remove")
-    public String removeFavorito(@RequestParam Long userId, @RequestParam String locationId) {
+    public String removeFavorito(@RequestParam(value = "user-id") Long userId, @RequestParam(value = "location-id") String locationId) {
+        return favoritoService.removeFavorito(userId, locationId);
+    }
+    
+    @DeleteMapping("/{userId}/remove/{locationId}")
+    public String removeUrlPathFavorito(@PathVariable("userId") Long userId, @PathVariable(value = "locationId") String locationId) {
         return favoritoService.removeFavorito(userId, locationId);
     }
 }
